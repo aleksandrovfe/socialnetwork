76f526a434aa92f7a206fdccaf15208803a32c21
alexandr <alexandrov6927@gmail.com>
2020-04-05 17:45:36 +0300

    Updates

---
+++ precache-manifest.eb0ccd24a5bc99f1fed8795215c75110.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38c6ff23c98f21f1b260ce8479fcd7f5",
+    "url": "/socialnetwork/index.html"
+  },
+  {
+    "revision": "c0028bfe9dc71e3f5a96",
+    "url": "/socialnetwork/static/css/main.b90dd07d.chunk.css"
+  },
+  {
+    "revision": "7da26c082f96086cb211",
+    "url": "/socialnetwork/static/js/2.4a69b4ca.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/socialnetwork/static/js/2.4a69b4ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0028bfe9dc71e3f5a96",
+    "url": "/socialnetwork/static/js/main.b2271015.chunk.js"
+  },
+  {
+    "revision": "b358f59b8da9462fd6a1",
+    "url": "/socialnetwork/static/js/runtime-main.7db4f6cb.js"
+  }
+]);
